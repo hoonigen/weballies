52a039d18b97bbb20c85d4aedb7f31bf6f913968
hoonigen <32509925+hoonigen@users.noreply.github.com>
2023-10-15 14:32:56 -0400

    Update mail-handler.php

--- mail-handler.php
+++ mail-handler.php
@@ -1,10 +1,8 @@
 <?php 
 
-    echo("Here I am");
-     echo "<script>console.log('Debug Objects' );</script>";
-    echo("Here I am");
-    header('Location: contact.html');
-    exit;
+   
+    header('Location: index.html');
+    
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     // You cannot use header and echo together. It's one or the other.
     
