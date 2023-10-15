82eacd54ed4ec3c2cb9f3e75ccdd79ba0ee5ef89
hoonigen <32509925+hoonigen@users.noreply.github.com>
2023-10-15 14:09:04 -0400

    Update mail-handler.php

--- mail-handler.php
+++ mail-handler.php
@@ -1,19 +1,7 @@
 <?php 
 if(isset($_POST['submit'])){
     echo("Here I am");
-    $to = "neenu.mariajohnson@gmail.com"; // this is your Email address
-    $from = "neenu.mariajohnson@gmail.com"; // this is the sender's Email address
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    echo("Here I am");
-    $subject = "Form submission";
-    $message = "Name : " . $name . "\n" . "Email : " . $email . "\n" . "Message : " . $_POST['message'];
-    echo("Here I am");
-
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    echo("Here I am");
-    mail($to,$subject,$message,$headers);
+    
     echo("Here I am");
     header('Location: contact.html');
     exit;
