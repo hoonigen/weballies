2ce03cfd487f62cfe0b5aae3128ebd802430d49e
hoonigen <32509925+hoonigen@users.noreply.github.com>
2023-10-15 14:45:33 -0400

    Update mail-handler.php

--- mail-handler.php
+++ mail-handler.php
@@ -1,7 +1,7 @@
 <?php 
 
    
-    header('Location: index.html/');
+    header("Location: index.html/");
     
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     // You cannot use header and echo together. It's one or the other.
