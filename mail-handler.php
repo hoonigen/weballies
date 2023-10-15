2d16f64425daf0948d60d20c1262e2a311e02b28
hoonigen <32509925+hoonigen@users.noreply.github.com>
2023-10-15 14:28:43 -0400

    Update mail-handler.php

--- mail-handler.php
+++ mail-handler.php
@@ -1,5 +1,5 @@
 <?php 
-if(isset($_POST['submit'])){
+
     echo("Here I am");
      echo "<script>console.log('Debug Objects' );</script>";
     echo("Here I am");
@@ -7,5 +7,5 @@ if(isset($_POST['submit'])){
     exit;
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     // You cannot use header and echo together. It's one or the other.
-    }
+    
 ?>
