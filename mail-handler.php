4645a94e99bf99ba6fa83f3e018989ca6d00a21d
hoonigen <32509925+hoonigen@users.noreply.github.com>
2023-10-15 14:06:18 -0400

    Update mail-handler.php

--- mail-handler.php
+++ mail-handler.php
@@ -1,17 +1,20 @@
 <?php 
 if(isset($_POST['submit'])){
+    echo("Here I am");
     $to = "neenu.mariajohnson@gmail.com"; // this is your Email address
     $from = "neenu.mariajohnson@gmail.com"; // this is the sender's Email address
     $name = $_POST['name'];
     $email = $_POST['email'];
-    
+    echo("Here I am");
     $subject = "Form submission";
     $message = "Name : " . $name . "\n" . "Email : " . $email . "\n" . "Message : " . $_POST['message'];
-
+    echo("Here I am");
 
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
+    echo("Here I am");
     mail($to,$subject,$message,$headers);
+    echo("Here I am");
     header('Location: contact.html');
     exit;
     // You can also use header('Location: thank_you.php'); to redirect to another page.
